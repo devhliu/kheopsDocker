10d5e22fd6dde0035127b944ec04cda4efdf14eb
Nicolas van Dooren <vandooren.nicolas@gmail.com>
2020-01-29 10:19:44 +0100

    A secure install

--- kheopsinstall.sh
+++ kheopsinstall.sh
@@ -17,13 +17,23 @@ fi
 secretpath="kheops/secrets/"
 kheopspath="kheops/"
 
+echo "What is the Keycloak realm ?"
+read KEYCLOAK_REALM
+
+echo "What is your host ?"
+read HOST
+
+sed -i "s|\%{kheops_realm}|$KEYCLOAK_REALM|g" ${kheopspath}/docker-compose.env
+sed -i "s|\%{kheops_host}|$HOST|g" ${kheopspath}/docker-compose.env
+sed -i "s|\%{kheops_host}|$HOST|g" ${kheopspath}/.env
+
 echo "Download project docker"
 if [[ ! -d "$kheopspath" ]]
 then
   mkdir $kheopspath
-  (cd $kheopspath && curl https://raw.githubusercontent.com/OsiriX-Foundation/kheopsDocker/install/kheops/.env --output .env --silent)
-  (cd $kheopspath && curl https://raw.githubusercontent.com/OsiriX-Foundation/kheopsDocker/install/kheops/docker-compose.env --output docker-compose.env --silent)
-  (cd $kheopspath && curl https://raw.githubusercontent.com/OsiriX-Foundation/kheopsDocker/install/kheops/docker-compose.yml --output docker-compose.yml --silent)
+  (cd $kheopspath && curl https://raw.githubusercontent.com/OsiriX-Foundation/kheopsDocker/install-secure/kheops/.env --output .env --silent)
+  (cd $kheopspath && curl https://raw.githubusercontent.com/OsiriX-Foundation/kheopsDocker/install-secure/kheops/docker-compose.env --output docker-compose.env --silent)
+  (cd $kheopspath && curl https://raw.githubusercontent.com/OsiriX-Foundation/kheopsDocker/install-secure/kheops/docker-compose.yml --output docker-compose.yml --silent)
 fi
 
 echo "Generate secrets"
@@ -37,12 +47,11 @@ secretfiles=("kheops_auth_hmasecret" "kheops_auth_hmasecret_post" \
   "kheops_metric_ressource_password" "kheops_superuser_hmasecret" \
   "keycloak_psql_password" "kheops_pacsdb_pass" "kheops_authdb_pass")
 
-echo "Enter the Keycloak administrator password (user: admin):"
-read KEYCLOAK_ADMIN_PASSWORD
-echo $KEYCLOAK_ADMIN_PASSWORD > ${secretpath}keycloak_admin_password
+echo "Enter the Keycloak client secret:"
+read KEYCLOAK_CLIENT_SECRET
+echo $KEYCLOAK_CLIENT_SECRET > ${secretpath}kheops_keycloak_clientsecret
 
 docker pull frapsoft/openssl
-docker pull andyneff/uuidgen
 for secretfile in ${secretfiles[*]}
 do
   secret=$(docker run -it frapsoft/openssl rand -base64 32)
@@ -51,29 +60,12 @@ do
   rm ${secretpath}tmp_${secretfile}
 done
 
-keycloakclientsecret=$(docker run -it andyneff/uuidgen)
-echo $keycloakclientsecret > ${secretpath}tmp_kheops_keycloak_clientsecret
-sed -e "s/\r//g" ${secretpath}tmp_kheops_keycloak_clientsecret > ${secretpath}kheops_keycloak_clientsecret
-rm ${secretpath}tmp_kheops_keycloak_clientsecret
-
 docker rm $(docker ps -a -q)
 docker rmi frapsoft/openssl
-docker rmi andyneff/uuidgen
 
 (cd $kheopspath && docker-compose down -v && docker-compose pull)
-(cd $kheopspath && docker-compose up -d keycloak)
-while true; do
-  status_code=$(curl --write-out "%{http_code}\n" --silent --output /dev/null "http://localhost:8080/auth/")
-  if [ $status_code = 500 ]
-  then
-    echo "Keycloak error"
-  elif [ $status_code != 000 ]
-  then
-    break;
-  fi
-  sleep 1
-done
 
-docker exec keycloak /keycloakconfiguration.sh
+echo "Add your public and private key in the directory kheops/secrets (fullchain1.pem / privkey1.pem)"
+read -p "Press enter to start KHEOPS"
 
 (cd $kheopspath && docker-compose up)
